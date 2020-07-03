61646fa3d34389d3e4d73ad465f2ffa82903a475
刘文天 <liuwt21@taikanglife.com>
2020-07-03 08:38:55 +0800

    update npm versions

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.all('*', function(req, res, next) {
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 // app.use(express.json());
